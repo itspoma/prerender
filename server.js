2cb71c84c50bf7e580aa1099066b850e3c96c647
itspoma <itspoma@gmail.com>
2017-04-20 17:18:11 +0300

    added fs-caching & response minify plugins

--- server.js
+++ server.js
@@ -2,19 +2,23 @@
 var prerender = require('./lib');
 
 var server = prerender({
-    workers: process.env.PRERENDER_NUM_WORKERS,
-    iterations: process.env.PRERENDER_NUM_ITERATIONS
+    port: 3000,
+    workers: process.env.PRERENDER_NUM_WORKERS || 1,
+    iterations: process.env.PRERENDER_NUM_ITERATIONS || 40
+    logRequests: true,
+    pageDoneCheckTimeout: 60000 // 1s
 });
 
-
-server.use(prerender.sendPrerenderHeader());
+// server.use(prerender.sendPrerenderHeader());
 // server.use(prerender.basicAuth());
 // server.use(prerender.whitelist());
-server.use(prerender.blacklist());
-// server.use(prerender.logger());
-server.use(prerender.removeScriptTags());
-server.use(prerender.httpHeaders());
+// server.use(prerender.blacklist());
+server.use(prerender.logger());
+// server.use(prerender.removeScriptTags());
+// server.use(prerender.httpHeaders());
 // server.use(prerender.inMemoryHtmlCache());
 // server.use(prerender.s3HtmlCache());
+server.use(prerender.minify());
+server.use(prerender.fsCaching());
 
 server.start();
